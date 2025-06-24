7241f69cb700a6ee28a40e822673dcedbf84254c
mrckie <marckie.js@gmail.com>
2025-06-24 13:36:39 +0800

    Implement Pokémon search, list, and views

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -9,43 +9,45 @@ const API_POKEMON_URL = process.env.API_POKEMON_URL;
 const API_BASE_URL = process.env.API_BASE_URL;
 
 router.get("/", async (req, res) => {
-  try {
-    const response = await axios.get(`${API_BASE_URL}/pokemon?limit=100000000000000`);
-    res.render("pages/home", { pokemonList: response.data.results });
-  } catch (e) {
-    res.status(500).send("Error fetching Pokémon data");
-  }
+	try {
+		const response = await axios.get(`${API_BASE_URL}/pokemon?limit=100000000000000`);
+		res.render("pages/home", { pokemonList: response.data.results });
+	} catch (e) {
+		res.status(500).send("Error fetching Pokémon data");
+	}
 });
 
 router.get("/pokemon/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    /**
-     * error checking
-     */
-    if (!id) {
-      return res.status(400).send("Pokémon ID is required");
-    }
-    const response = await axios.get(`${API_BASE_URL}/pokemon/${id}`);
-    res.render("pages/details", { pokemon: response.data });
-  } catch (e) {
-    console.log(e);
-    res.status(500).send("Error fetching Pokémon details");
-  }
+	try {
+		const { id } = req.params;
+		/**
+		 * error checking
+		 */
+		if (!id) {
+			return res.status(400).send("Pokémon ID is required");
+		}
+		const response = await axios.get(`${API_BASE_URL}/pokemon/${id}`);
+		res.render("pages/details", { pokemon: response.data });
+	} catch (e) {
+		console.log(e);
+		res.status(500).send("Pokémon not found");
+	}
 });
 
 
-router.get("/search", async (req, res) => {
-    try {
-        const { name } = req.query;
-        if (!name) return res.redirect("/");
 
-        const response = await axios.get(`${API_BASE_URL}/pokemon/${name}`);
-        res.render("pages/details", { pokemon: response.data });
-    } catch (e) {
-        console.log(e);
-        res.status(500).send("Error fetching Pokémon details");
-    }
+
+router.get("/search", async (req, res) => {
+	try {
+		const { name } = req.query;
+		if (!name) return res.redirect("/");
+
+		const response = await axios.get(`${API_BASE_URL}/pokemon/${name}`);
+		res.render("pages/details", { pokemon: response.data });
+	} catch (e) {
+		console.log(e);
+		res.status(500).send("Pokémon not found");
+	}
 })
 
 export default router;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import e from "express";
@@ -7,10 +8,13 @@ const app = e();
 const port = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 app.use("/", pokemonRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
-})
+});
